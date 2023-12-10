b2630466db7c6bb5fe49dcb43f7d62930d32eac4
TheFimerHub <m.stepanov@it-park.tech>
2023-12-10 16:25:33 +0300

    Homework_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
--- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id SERIAL PRIMARY KEY,
+    first_name  VARCHAR(64)  NOT NULL,
+    last_name   VARCHAR(64)  NOT NULL,
+    title       VARCHAR(255) NOT NULL,
+    birth_date  DATE,
+    notes       TEXT
+);
+
+CREATE TABLE customers
+(
+    customer_id  VARCHAR(5) PRIMARY KEY,
+    company_name VARCHAR(255) NOT NULL,
+    contact_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id    SERIAL PRIMARY KEY,
+    customer_id VARCHAR(5) NOT NULL,
+    employee_id INT        NOT NULL,
+    order_date  DATE       NOT NULL,
+    ship_city   VARCHAR(255),
+    FOREIGN KEY (customer_id) REFERENCES customers (customer_id),
+    FOREIGN KEY (employee_id) REFERENCES employees (employee_id)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,82 @@
-"""Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+import psycopg2  # type: ignore
+
+
+def connect():
+    try:
+        connection = psycopg2.connect(
+            host="localhost",
+            database="north",
+            user="postgres",
+            password="minilam123"
+        )
+        return connection
+    except psycopg2.Error as e:
+        print("Невозможно подключиться к базе данных")
+        print(e)
+        return None
+
+def fill_tables(connection):
+    try:
+        cursor = connection.cursor()
+
+        # Заполнение employees из CSV
+        with open('north_data/employees_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Пропустить заголовок
+            for row in reader:
+                print(row)
+                cursor.execute(
+                    "INSERT INTO employees (first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s)",
+                    (row[1], row[2], row[3], row[4], row[5]))
+
+        # Заполнение customers из CSV
+        with open('north_data/customers_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Пропустить заголовок
+            for row in reader:
+                print(row)
+                cursor.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                               (row[0], row[1], row[2]))
+
+        # Заполнение orders из CSV
+        with open('north_data/orders_data.csv', 'r') as file:
+            reader = csv.reader(file)
+            next(reader)  # Пропустить заголовок
+            for row in reader:
+                print(row)
+                cursor.execute(
+                    "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                    (row[0], row[1], row[2], row[3], row[4]))
+
+        connection.commit()
+        cursor.close()
+
+    except psycopg2.Error as e:
+        print("Error filling tables")
+        print(e)
+
+
+# def create_tables(connection):
+#     try:
+#         cursor = connection.cursor()
+#         create_tables_sql = ...
+#         cursor.execute(create_tables_sql)
+#         connection.commit()
+#         cursor.close()
+#     except psycopg2.Error as e:
+#         print("Ошибка создании таблицы")
+#         print(e)
+
+
+def main():
+    connection = connect()
+
+    if connection:
+        fill_tables(connection)
+
+        connection.close()
+
+
+if __name__ == "__main__":
+    main()
